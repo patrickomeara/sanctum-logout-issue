d77015bd40a29b3a30b00ab13c968f1b0cd75dd0
Patrick O'Meara <pat@fixd.io>
2023-08-11 19:39:03 +1000

    Use auth.session middleware

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['auth:sanctum'])
+Route::middleware(['auth:sanctum', 'auth.session'])
     ->group(function () {
         Route::get('user', function (Request $request) {
             return $request->user();
